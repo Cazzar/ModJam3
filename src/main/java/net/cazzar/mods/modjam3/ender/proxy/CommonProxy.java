d1193cc19c1cc0c64e42c971c2d53bbde3c26385
Cayde Dixon <cayde.dixon@gmail.com>
2013-12-13 17:13:52 +1100

    More work, second commit

--- src/main/java/net/cazzar/mods/modjam3/ender/proxy/CommonProxy.java
+++ src/main/java/net/cazzar/mods/modjam3/ender/proxy/CommonProxy.java
@@ -1,7 +1,10 @@
 package net.cazzar.mods.modjam3.ender.proxy;
 
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.cazzar.mods.modjam3.ender.blocks.BlockEnderTeleporter;
 import net.cazzar.mods.modjam3.ender.common.Config;
 import net.cazzar.mods.modjam3.ender.common.EnderCreativeTab;
+import net.minecraftforge.common.Configuration;
 
 import java.io.File;
 
@@ -12,11 +15,20 @@ public class CommonProxy {
     public EnderCreativeTab tab = new EnderCreativeTab();
     public Config config;
 
-    public void initConfig(File configFile) {
+    public static class Blocks {
+        public static BlockEnderTeleporter enderTeleporter;
+    }
 
+    public void initConfig(File configFile) {
+        Configuration configuration = new Configuration(configFile);
+        config = new Config();
+
+        config.teleporterID = configuration.get("block", "teleporterID", config.teleporterID).getInt();
     }
 
     public void initBlocks() {
+        if (config == null) throw new RuntimeException("initConfig() has not been called yet.");
 
+        GameRegistry.registerBlock(Blocks.enderTeleporter = new BlockEnderTeleporter(config.teleporterID), "enderTeleporter");
     }
 }
